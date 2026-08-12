8c02be1e1bf88aceedd1608d53ffdfe1dc87b192
senkatto <senkatto@users.noreply.github.com>
2026-08-13 00:57:06 +0300

    release: Warpy 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -168,8 +168,8 @@ android {
         applicationId = "com.warpy.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.0.2"
+        versionCode = 21
+        versionName = "1.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += "arm64-v8a"
